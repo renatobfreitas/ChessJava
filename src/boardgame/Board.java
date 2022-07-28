6a93fb0ab71c1508ca0811b9d7e996aa139c253d
Renato Freitas <renatofreitas.dev@gmail.com>
2022-07-28 16:22:39 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -51,6 +51,18 @@ public class Board {
 		pieces[position.getRow()][position.getColum()] = piece;
 		piece.position = position;
 	}
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board!");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColum()] = null;
+		return aux;
+	}
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -63,4 +75,5 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
 }
